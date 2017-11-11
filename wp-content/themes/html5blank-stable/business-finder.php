773bddef29aa6ff26657abfa6f4b4e2934700a79
halfcursive <Kevon@Kevons-MBP.lehman.edu>
2017-11-10 23:19:11 -0500

    Added business-finder

---
+++ wp-content/themes/html5blank-stable/business-finder.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Business Finder </title>
+</head>
+<body>
+<h1>Business Finder</h1>
+<nav>
+<h4>Resources</h4>
+
+<h4>Best Practices</h4>
+<p>What Is a Best Practice?
+A best practice is the process of finding and using ideas and strategies from outside your company and industry to improve performance in any given area.
+
+Big businesses have used best practice benchmarking over decades and realized billions in savings and revenues in all areas of business operations and sales. Small business can reap even greater rewards from best practices.
+<ul>
+<u>Benefits of Best Practices for Small Business</u>:
+<li>Reduce Costs: Small companies often do not have the deep financial pockets of big business to "re-invent the wheel." By learning what other companies have successfully done, a small business can save money without testing new ideas.</li>
+<li>Avoid Mistakes: Solving business problems on your own can result in costly errors. Learning what others have done can keep your business in business.</li>
+<li>Find New Ideas: Adopting the "not-invented-here" attitude can spell disaster for small businesses. Learn to borrow the best from beyond your company.</li>
+<li>Improve Performance: When your business looks for best practices outside your business, a wonderful thing happens. You raise the bar of performance and set new standards of excellence to propel your company forward.</li></ul></p>
+
+<h4>Mentorships</h4>
+<ul>
+	<p><li>SCORE (www.score.org) is probably the best-known organization providing free (and confidential) mentoring to small business owners via its national network of some 13,000 retired business executives, leaders and volunteers. SCORE’s volunteer mentors share their expertise through both in-person and online counseling (via email).</li>
+
+	<li>Small Business Development Centers (SBDCs) are another great source of free or low-cost help and advice for current and would-be business owners of all types in all locations. There are over 1,200 SBDC locations nationwide. For help locating one, visit www.asbdc-us.org.</li>
+
+	<li>Women’s Business Centers (WBCs) offer business training, counseling and other resources to help women start and grow successful businesses. To find your nearest WBC check the Office of Women’s Business Ownership at www.sba.gov.</li>
+
+	<li>Minority Business Development Centers, part of the U.S. Department of Commerce, offer free help to minority-owned businesses through about 40 centers nationwide.  Visit the Minority Business Development Agency at www.mbda.gov.</li>
+
+	<li>Trade or Professional Associations: Many trade and professional associations operate mentoring programs for business owners just starting out. Some offer formal one-on-one mentoring sessions as well as group networking opportunities.  Check associations in your industry.</li>
+
+	<li>Mentors for Government Contracting: If your business plans to sell to the federal government, the General Services Administration (GSA) offers a Mentor/Protégé Program designed to encourage prime contractors to help small businesses be more successful in government contracting and enhance their ability to perform successfully on government contracts and subcontracts. You’ll find it at GSA.gov.</li>
+
+	<li>Your Own Network: Who do you know? Is there a previous boss who was very inspiring to you or a friend who is a business owner? Ask that person to be your mentor or share his or her successes and struggles. You have nothing to lose. Just be prepared to share with them why you chose them in particular, your goals and what you are looking for from them. The best way to connect these days is LinkedIn. Make sure you’re on it!</li></p>	
+</ul>
+
+
+<h4>Outreach</h4>
+<p> Work with schools, NGO's (non-governmental organizations), team up with other businesses</p>
+
+<h4>Funding</h4>
+</nav>
+<aside>Categories</aside>
+
+
+<div id="map" style="width:400px;height:400px;background:yellow"></div>
+
+<script>
+function myMap() {
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: new google.maps.LatLng(40.7128, -74.0060), zoom: 10
+  };
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+}
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCfx7ibUldGtCuS0JUo6We90hYRlQQpHoc&callback=myMap"></script>
+
+
+</body>
+</html>
